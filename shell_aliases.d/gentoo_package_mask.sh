d2b8a405d0c0703e6b7095f39abf0531fa2b901e
Marcin Nowicki <pr0d1r2@gmail.com>
2017-01-11 15:17:32 +0100

    Support fully copied lines

--- shell_aliases.d/gentoo_package_mask.sh
+++ shell_aliases.d/gentoo_package_mask.sh
@@ -11,7 +11,7 @@
 #                                 < 45.6.0                  >= 45.6.0"
 function gentoo_package_mask() {
   case $@ in
-    1\ \ *)
+    1\ \ * | \ 1\ \ * | \ \ 1\ \ *)
       local gentoo_package_mask_LINE
       local gentoo_package_mask_LINE_NUM
       for gentoo_package_mask_LINE_NUM in 1 2 3 4 5 6 7 8 9
