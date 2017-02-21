addc61b77d14f12536c4f0af60796579fc43a35f
Marcin Nowicki <pr0d1r2@gmail.com>
2017-02-21 08:40:02 +0100

    Remove file before refresh

--- shell_aliases.d/gentoo_package_mask_recipe_refresh.sh
+++ shell_aliases.d/gentoo_package_mask_recipe_refresh.sh
@@ -1,6 +1,6 @@
 function gentoo_package_mask_recipe_refresh() {
   cd $HOME/projects/gentoo_package_masking/recipes || return $?
-  echo -n > default.rb || return $?
+  rm -f default.rb || return $?
   local gentoo_package_mask_RECIPE
   for gentoo_package_mask_RECIPE in `find . -type f -name "*.rb" | grep -v "./default.rb" | sort | sed -e 's/^./gentoo_package_masking/' | sed -e 's/\//__/g' | sed -e 's/gentoo_package_masking__/gentoo_package_masking::/' | sed -e 's/.rb$//g'`
   do
