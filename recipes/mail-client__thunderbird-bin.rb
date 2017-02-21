d22b693e9c6a0f6306ed8bb8b0bf4097300331eb
Marcin Nowicki <pr0d1r2@gmail.com>
2017-02-21 10:06:20 +0100

    GLSA 201702-13

--- recipes/mail-client__thunderbird-bin.rb
+++ recipes/mail-client__thunderbird-bin.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<mail-client/thunderbird-bin-45.6.0'
+gentoo_package_mask '<mail-client/thunderbird-bin-45.7.0'
--- recipes/mail-client__thunderbird.rb
+++ recipes/mail-client__thunderbird.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<mail-client/thunderbird-45.6.0'
+gentoo_package_mask '<mail-client/thunderbird-45.7.0'
