18e4b08d9aba5fc0445f9e98d08a3418b75950a4
Marcin Nowicki <pr0d1r2@gmail.com>
2017-08-21 06:46:35 +0200

    GLSA 201708-08

---
+++ recipes/app-arch__bzip2.rb
@@ -0,0 +1,1 @@
+gentoo_package_mask '<app-arch/bzip2-1.0.6-r8'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 include_recipe 'gentoo_package_masking::app-admin__ansible'
 include_recipe 'gentoo_package_masking::app-admin__lsyncd'
 include_recipe 'gentoo_package_masking::app-admin__sudo'
+include_recipe 'gentoo_package_masking::app-arch__bzip2'
 include_recipe 'gentoo_package_masking::app-arch__libarchive'
 include_recipe 'gentoo_package_masking::app-arch__p7zip'
 include_recipe 'gentoo_package_masking::app-editors__gvim'
