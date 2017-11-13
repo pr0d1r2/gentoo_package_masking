acc6ce1ffaa0bca61f729fc840f98448b321529a
Marcin Nowicki <pr0d1r2@gmail.com>
2017-11-13 04:05:15 +0100

    GLSA 201711-02

--- recipes/www-client__chromium.rb
+++ recipes/www-client__chromium.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<www-client/chromium-62.0.3202.62'
+gentoo_package_mask '<www-client/chromium-62.0.3202.89'
--- recipes/www-client__google-chrome.rb
+++ recipes/www-client__google-chrome.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<www-client/google-chrome-62.0.3202.62'
+gentoo_package_mask '<www-client/google-chrome-62.0.3202.89'
