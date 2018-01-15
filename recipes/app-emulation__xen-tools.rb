ff023bfa0707924ad9b862a24552afd08c94c56b
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-15 07:08:08 +0100

    GLSA 201801-14

--- recipes/app-emulation__xen-tools.rb
+++ recipes/app-emulation__xen-tools.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<app-emulation/xen-tools-4.7.3'
+gentoo_package_mask '<app-emulation/xen-tools-4.9.1-r1'
--- recipes/app-emulation__xen.rb
+++ recipes/app-emulation__xen.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<app-emulation/xen-4.7.3'
+gentoo_package_mask '<app-emulation/xen-4.9.1-r1'
