eb602b3fda33fd160e78eaf63d080454429092e4
Marcin Nowicki <pr0d1r2@gmail.com>
2018-11-12 17:28:38 +0100

    GLSA 201811-04

--- recipes/www-client__firefox-bin.rb
+++ recipes/www-client__firefox-bin.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<www-client/firefox-bin-60.2.2'
+gentoo_package_mask '<www-client/firefox-bin-60.3.0'
--- recipes/www-client__firefox.rb
+++ recipes/www-client__firefox.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<www-client/firefox-60.2.2'
+gentoo_package_mask '<www-client/firefox-60.3.0'
