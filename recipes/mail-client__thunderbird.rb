67182da3d84f45e775bd08cf330bc6e29faaf9dd
Marcin Nowicki <pr0d1r2@gmail.com>
2019-04-04 09:39:03 +0200

    GLSA 201904-07

--- recipes/mail-client__thunderbird.rb
+++ recipes/mail-client__thunderbird.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<mail-client/thunderbird-60.3.0'
+gentoo_package_mask '<mail-client/thunderbird-60.6.1'
--- recipes/www-client__firefox-bin.rb
+++ recipes/www-client__firefox-bin.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<www-client/firefox-bin-60.5.1'
+gentoo_package_mask '<www-client/firefox-bin-60.6.1'
