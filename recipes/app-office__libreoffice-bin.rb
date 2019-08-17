78d055b6e6e162c4605e082a3f1972f00975ec84
Marcin Nowicki <pr0d1r2@gmail.com>
2019-08-17 16:48:29 +0200

    GLSA 201908-13

--- recipes/app-office__libreoffice-bin.rb
+++ recipes/app-office__libreoffice-bin.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<app-office/libreoffice-bin-5.4.5.1'
+gentoo_package_mask '<app-office/libreoffice-bin-6.2.5.2'
--- recipes/app-office__libreoffice.rb
+++ recipes/app-office__libreoffice.rb
@@ -1,1 +1,1 @@
-gentoo_package_mask '<app-office/libreoffice-5.4.5.1'
+gentoo_package_mask '<app-office/libreoffice-6.2.5.2'
